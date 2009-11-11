c7aeec9e15561f8ce99219adfb8a293813dece57
izso <zsolt.istvan@gnx.net>
2009-11-11 12:25:07 +0200

    CoedFile and CoedProject are now implemented / however, with basic functionalities

--- Base/src/coed/base/data/CoedFile.java
+++ Base/src/coed/base/data/CoedFile.java
@@ -7,14 +7,62 @@ import java.io.File;
  * @author Neobi
  *
  */
-public class CoedFile { //TODO: create real class instead of abstract
+public class CoedFile { 
+    /**
+	 * Relative path of the file in its project
+	 */
+    private String path;
+    
+    /**
+     * Project of the file (its name)
+     */
+    private String project;
+    
+    /**
+     * Current version of the file
+     */
+    private String version;
+    
+    /**
+     * All existing vwersions of the file
+     */
+    private String[] allVersions;
+    
+    //CONSTRUCTORS----------------------------------------
+    
+    public CoedFile(String project, String path, String version, String[] allVersions){
+    	this.project=project;
+    	this.path=path;
+    	this.version=version;
+    	this.allVersions=allVersions;
+    }
+    
+    public CoedFile(String project, String path, String version){
+    	this(project,path,version,null);
+    }
+    
+    public CoedFile(String project, String path){
+    	this(project,path,null,null);
+    }
+    
+    //INSTANCE METHODS-------------------------------------
    
-   public CoedFile(String path) {}
-   public CoedFile(String project, String path, String version, String[] allVersions){}
-   
-   public String getProject() { return null;}
-   public String getPath() {return null;}
-   public String getVersion() {return null;}
-   public String[] getAllVersions() {return null;}
-   public File getFile() {return null;}
+    public String getProject() { 
+    	return this.project;
+    }
+    
+    public String getPath() {
+    	return this.path;
+    }
+    public String getVersion() {
+    	return this.version;
+    }
+    
+    public String[] getAllVersions() {
+    	return this.allVersions;
+    }
+    
+    public File getFile() {
+    	return FileManager.getFileFor(this);
+    }
 }
--- Base/src/coed/base/data/CoedProject.java
+++ Base/src/coed/base/data/CoedProject.java
@@ -8,10 +8,32 @@ package coed.base.data;
 public class CoedProject { 
    
    private String name;
+   private String[] collaborators;
    
-   public CoedProject(){
-	   
+   public CoedProject(String name){
+	   this.name=name;
+	   this.collaborators=null;
    }
    
+   public CoedProject(String name, String[] collaborators){
+	   this.name=name;
+	   this.collaborators=collaborators;
+   }
+   
+   public String getName(){
+	   return name;
+   }
+   
+   public void setName(String name){
+	   this.name=name;
+   }
+   
+   public String[] getCollaborators(){
+	   return this.collaborators;
+   }
+   
+   public void setCollaborators(String[] collaborators){
+	   this.collaborators=collaborators;
+   }
    
 }
