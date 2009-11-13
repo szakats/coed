b7d2e25373b39ff9d49a3224828d91467e74c12d
izso <zsolt.istvan@gmx.net>
2009-11-14 00:14:19 +0200

    Exception for invalid config file location

---
+++ Base/src/coed/base/data/exceptions/InvalidConfigFileException.java
@@ -0,0 +1,12 @@
+package coed.base.data.exceptions;
+
+import java.io.IOException;
+
+public class InvalidConfigFileException extends IOException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
