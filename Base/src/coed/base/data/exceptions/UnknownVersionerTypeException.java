3992d2ca629be3d300d5ffc7d7ab0aa8f2b8404c
izso <zsolt.istvan@gmx.net>
2009-11-14 00:14:00 +0200

    Moved this exception out of the communicator factory, because it was not reachable.

---
+++ Base/src/coed/base/data/exceptions/UnknownVersionerTypeException.java
@@ -0,0 +1,10 @@
+package coed.base.data.exceptions;
+
+public class UnknownVersionerTypeException extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public UnknownVersionerTypeException() {}
+}
