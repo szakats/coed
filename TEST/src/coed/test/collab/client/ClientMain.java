c3d6c815030a7f374c51cd44427a850bcecd4575
szakats <szakats@coed.org>
2009-11-14 12:06:40 +0200

    Added TEST project with tests for config and client.

---
+++ TEST/src/coed/test/collab/client/ClientMain.java
@@ -0,0 +1,15 @@
+package coed.test.collab.client;
+
+import coed.collab.client.CollaboratorClient;
+
+public class ClientMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CollaboratorClient collab;
+		collab = new CollaboratorClient(null);
+	}
+
+}
---
+++ TEST/src/coed/test/collab/config/ConfigTest.java
@@ -0,0 +1,15 @@
+package coed.test.collab.config;
+
+import coed.base.data.exceptions.InvalidConfigFileException;
+import coed.collab.client.config.Config;
+
+public class ConfigTest {
+	public static void main (String args[]) {
+		try {
+			Config c = new Config("D:\\University\\III YEAR\\1st Semester\\SE\\CoED\\coed\\CoedCollaboration\\src\\coed\\collab\\client\\config\\myfile.txt");
+			System.out.println(c.getString("host"));
+		} catch(InvalidConfigFileException e) {
+			e.printStackTrace();
+		}
+	}
+}
