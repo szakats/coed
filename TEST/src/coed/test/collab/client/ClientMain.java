e67cb1ea35a14e3c92fa1f2656b1d586694231d1
neobi008 <neobi008@yahoo.com>
2009-12-29 14:46:00 +0200

    Test

--- TEST/src/coed/test/collab/client/ClientMain.java
+++ TEST/src/coed/test/collab/client/ClientMain.java
@@ -5,10 +5,12 @@ import java.util.concurrent.ExecutionException;
 import coed.base.comm.ICoedCollaborator;
 import coed.base.comm.ICollabStateListener;
 import coed.base.data.CoedObject;
+import coed.base.data.TextModification;
 import coed.base.data.exceptions.NotConnectedException;
 import coed.base.util.IFutureListener;
 import coed.collab.client.CollaboratorClient;
 import coed.collab.connection.ICoedConnection;
+import coed.collab.protocol.AuthentificationMsg;
 import coed.collab.protocol.GoOnlineMsg;
 import coed.collab.protocol.SendChangesMsg;
 import coed.versioning.client.StaticVersioner;
@@ -24,6 +26,7 @@ public class ClientMain {
 			if(to == ICoedCollaborator.STATUS_CONNECTED) {
 				//ICoedConnection conn = collab.getConn();
 				//assert conn != null;
+				collab.getConn().send(new AuthentificationMsg("user1"));
 
 				new GoOnlineTest();
 			}
@@ -49,6 +52,28 @@ public class ClientMain {
 		@Override
 		public void got(String result) {
 			System.out.println("received contents: " + result);
+			new SendChangesTest();
+		}
+  
+		@Override
+		public void caught(Throwable e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	class SendChangesTest implements IFutureListener<String> {
+		
+		SendChangesTest() {
+			System.out.println("sending changes");
+			TextModification[] mods = new TextModification[1];
+			mods[0] = new TextModification(1,3,"333","user1");
+			collab.getConn().send(new SendChangesMsg("file",mods));
+		}
+
+		@Override
+		public void got(String result) {
+			System.out.println("sent contents client1 " + result);
 		}
   
 		@Override
