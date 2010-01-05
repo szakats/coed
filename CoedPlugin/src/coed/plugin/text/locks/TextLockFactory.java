d808c4173f8cc37aff24c8eddd6554664f528069
izso <zsolt.istvan@gmx.net>
2010-01-05 11:09:18 +0200

    Partial commit for lock managers... TODO: comments TODO: do smthg when lock fails in controller TODO: make quelque chose with Protetctive Manager

--- CoedPlugin/src/coed/plugin/text/locks/TextLockFactory.java
+++ CoedPlugin/src/coed/plugin/text/locks/TextLockFactory.java
@@ -3,15 +3,22 @@ package coed.plugin.text.locks;
 import coed.base.data.exceptions.InvalidConfigFileException;
 
 public class TextLockFactory {
-	public static final String OPTIMISTIC_MANAGER="optimistic";
+	public static final String BASIC_MANAGER="basic";
 	public static final String PROTECTIVE_MANAGER="protective";
 
 	public static ITextLockManager getManagerFor(String type) throws InvalidConfigFileException{
 		return getManagerFor(type,0);
 	}
 	
-	public static ITextLockManager getManagerFor(String type, Integer releaseTimeout) throws InvalidConfigFileException{
-		return null;
+	public static ITextLockManager getManagerFor(String type, Integer releaseDelay) throws InvalidConfigFileException{
+		if (type==BASIC_MANAGER){
+			return new BasicTextLockManager(releaseDelay);
+		} else if (type==PROTECTIVE_MANAGER){
+			return new ProtectiveTextLockManager(releaseDelay);
+		} else {
+			//sorry. something is not clear...
+			throw new InvalidConfigFileException();
+		}
 	}
 	
 }
